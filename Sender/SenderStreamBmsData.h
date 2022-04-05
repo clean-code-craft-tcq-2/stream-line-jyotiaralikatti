34042ca808bd2d58e469f2f7e40faeea29d4f3c2
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-04-06 02:07:31 +0530

    Update SenderStreamBmsData.h

--- Sender/SenderStreamBmsData.h
+++ Sender/SenderStreamBmsData.h
@@ -1,1 +1,17 @@
+#pragma once
 
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define NUMBER_OF_MAX_DATA 50
+
+typedef enum
+{
+	E_NOT_OK,
+	E_OK
+}StatusType;
+
+StatusType SenderDataRead(float* , float* );
+void SendDataToConsole(float Temperature[], float StateOfCharge[]);
+StatusType BMS_Sender();
