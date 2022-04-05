d69b8395937276b6cb270069d9a5d61972f13cea
jyotiaralikatti <48976964+jyotiaralikatti@users.noreply.github.com>
2022-04-06 02:38:05 +0530

    Update TestSenderStreamData.cpp

--- TestSenderStreamData.cpp
+++ TestSenderStreamData.cpp
@@ -7,15 +7,14 @@ SCENARIO("Read the Sender datas for Temperature and StateofCharge from file")
 {
     GIVEN("Read input from file SenderData.txt")
     {
-        float Temperature[NUMBER_OF_MAX_DATA] = {0};
-        float StateOfCharge[NUMBER_OF_MAX_DATA] = {0};
-
         WHEN("Having Input data in file")
         {
+            float Temperature[NUMBER_OF_MAX_DATA] = {0};
+            float StateOfCharge[NUMBER_OF_MAX_DATA] = {0};
             //StatusType status_check  = E_NOT_OK;
             THEN("Check weather read of data from file is successful")
             {
-               REQUIRE(SenderDataRead (Temperature,StateOfCharge) == E_OK);
+               REQUIRE(SenderDataRead(Temperature,StateOfCharge) == E_OK);
             }
         }
     }
