dc65d7ae7e1b8c30bf3c2a42b6119b3073a027b5
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-13 20:25:31 +0530

    Update ReceiverStreamBmsData.c

--- Receiver/ReceiverStreamBmsData.c
+++ Receiver/ReceiverStreamBmsData.c
@@ -1,48 +1,63 @@
 #include "ReceiverStreamBmsData.h"
 
-int receiveBmsDataFromConsole(){
-  float input1[300];
-  float input2[300];
-  //scanf("%f",&input);
-  //printf("%f, %f, %f, %f",input[0],input[1],input[2],input[3]);
+int receiveBmsDataFromConsole(float* Temperature, float* Soc){
   
-  char unusedData[300];
-    for(int j = 0; j < 6; j++){
-       
-        scanf("%f", &input1[j]);     
-        //scanf("%50s", unusedData);
-        scanf("%50s", unusedData);
-        scanf("%f", &input2[j]);
-      /*if(scanf("%50s", unusedData) == EOF) 
+  char dicardData[300];
+  for(int bufferIndex = 0; bufferIndex < 6; bufferIndex++){
+    scanf("%f", &Temperature[bufferIndex]);     
+    scanf("%50s", dicardData);
+    scanf("%f", &soc[bufferIndex]);
+    /*if(scanf("%50s", unusedData) == EOF) 
         {
             break;
         } */
-        printf("%f, %f\n",input1[j],input2[j]);
+    printf("%f, %f\n",Temperature[bufferIndex],Soc[bufferIndex]);
     }
   
   return 1;
 }
 
-int findMaxValueFromBmsConsole(char* inputArray){
+
+static int intCompare(const void *previousBufferElement, const void *currentBufferElement)
+{
+    float int_previousBufferElement = * ( (float*) previousBufferElement );
+    float int_currentBufferElement = * ( (float*) currentBufferElement );
+
+    if ( int_previousBufferElement == int_currentBufferElement ) return 0;
+    else if ( int_previousBufferElement < int_currentBufferElement ) return -1;
+    else return 1;
+}
+ void MinMaxSortFunc(float* InputArray)
+ {
+   qsort(inputArray, 50, sizeof(float), intCompare);
+ }
+
+float findMinMaxValueTemperatureFromBmsSender(float* inputArray){
+  MinMaxSortFunc(inputArray);
+  return inputArray[0];
+}
+
+float findMinMaxValueSocFromBmsSender(float* inputArray){
+  MinMaxSortFunc(inputArray);
+  return inputArray[0];
+}
+float findMovingAverageFromBmsData(float* inputArray){
   return 1;
 }
 
-int findMinValueFromBmsConsole(char* inputArray){
-  return 1;
-}
-int findMovingAverageFromBmsData(char* inputArray){
-  return 1;
-}
-
-void printToConsoleMinMaxAndMovingAverage(char min, char max, char movingAverage){
+void printToConsoleMinMaxAndMovingAverage(float min, float max, int movingAverage){
   
   cout << "Min Value is : " << min << " Max Value is : " << max <<" Moving Average is : " << movingAverage <<endl;
 }
 
-/*processReceivedBmsStreamData(){
-  char *receiverBuffer = receiveBmsDataFromConsole();
-  char max = findMaxValueFromBmsConsole(receiverBuffer);
-  char min = findMinValueFromBmsConsole(receiverBuffer);
-  char movingAverage = findMovingAverageFromBmsData(receiverBuffer);
-  printToConsoleMinMaxAndMovingAverage(min, max,movingAverage);
-*/
+int processReceivedBmsStreamData(){
+  float Temperature[300];
+  float Soc[300];  
+  int status = receiveBmsDataFromConsole(Temperature, Soc);
+  float minTemp = findMinMaxValueTemperatureFromBmsSender(Temperature);
+  float minSoc = findMinMaxValueSocFromBmsSender(Soc);
+ // char movingAverage = findMovingAverageFromBmsData(receiverBuffer);
+  printToConsoleMinMaxAndMovingAverage(minTemp, minSoc,1);
+  return 1;
+}
+
