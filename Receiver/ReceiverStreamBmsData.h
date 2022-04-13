7312223cbaa25390a2965a76c390d52ed9bd0d13
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-13 12:29:06 +0530

    Create  ReceiverStreamBmsData.h

---
+++ Receiver/ReceiverStreamBmsData.h
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sstream> // for ostringstream
+#include <string.h>
+#include<array>
+#include <fstream>
+using namespace std;
+#define MAX_BMS_READ 50
+
+int receiveBmsDataFromConsole();
