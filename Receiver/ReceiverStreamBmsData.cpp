1b59a1081a8a5ed82a632a9feb25b8c4dfea3268
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-18 18:43:32 +0530

    Update ReceiverStreamBmsData.cpp

--- Receiver/ReceiverStreamBmsData.cpp
+++ Receiver/ReceiverStreamBmsData.cpp
@@ -22,7 +22,7 @@ int cmpfunc (const void * int_previousBufferElement, const void * int_currentBuf
 
  float* MinMaxSortFunc(float* InputArray)
  {
-   sort (InputArray, InputArray+48, cmpfunc);
+   sort (InputArray, InputArray+50, cmpfunc);
    //qsort(InputArray, 48, sizeof(float), cmpfunc);
    return InputArray;
  }
