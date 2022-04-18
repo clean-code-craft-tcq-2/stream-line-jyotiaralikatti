656398b50951113512a68e242ede7beb60f8c689
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-18 20:18:56 +0530

    Update testReceiverStreamData.cpp

--- testReceiverStreamData.cpp
+++ testReceiverStreamData.cpp
@@ -3,11 +3,11 @@
 #include "test/catch.hpp"
 #include "Receiver/ReceiverStreamBmsData.h"
 
-TEST_CASE("Check whether sensor inputs are read from sender file") {
+/*TEST_CASE("Check whether sensor inputs are read from sender file") {
   
   REQUIRE((receiveBmsDataFromConsole().status) == 1);
   
-}
+}*/
 
 TEST_CASE("Process sensor input and output in CSV format") {
   REQUIRE(processReceivedBmsStreamData() == 1);
