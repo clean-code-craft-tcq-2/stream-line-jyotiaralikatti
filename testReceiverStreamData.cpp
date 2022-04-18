5b9ddf9a8b411acce5c094c2c77356696dc26c97
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-18 20:51:23 +0530

    Update testReceiverStreamData.cpp

--- testReceiverStreamData.cpp
+++ testReceiverStreamData.cpp
@@ -3,12 +3,14 @@
 #include "test/catch.hpp"
 #include "Receiver/ReceiverStreamBmsData.h"
 
-/*TEST_CASE("Check whether sensor inputs are read from sender file") {
-  
-  REQUIRE((receiveBmsDataFromConsole().status) == 1);
-  
-}*/
+
 
 TEST_CASE("Process sensor input and output in CSV format") {
   REQUIRE(processReceivedBmsStreamData() == 1);
+}
+
+TEST_CASE("Check whether sensor inputs are read from sender file") {
+  float InArray = {2,4,5,6,7,8,9,10,12,3,4,5,6,7,8,7}; 
+  REQUIRE((findMovingAverageFromBmsData(InArray,16)) == 6);
+  
 }
