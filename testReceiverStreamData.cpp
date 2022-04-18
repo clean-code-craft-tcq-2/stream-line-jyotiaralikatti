88dc31fa3661bcd7cba8da9b504714df81dd07d0
AparnaHariharadas <90903197+AparnaHariharadas@users.noreply.github.com>
2022-04-18 20:58:15 +0530

    Update testReceiverStreamData.cpp

--- testReceiverStreamData.cpp
+++ testReceiverStreamData.cpp
@@ -10,7 +10,7 @@ TEST_CASE("Process sensor input and output in CSV format") {
 }
 
 TEST_CASE("Check whether sensor inputs are read from sender file") {
-  float InArray = {2,4,5,6,7,8,9,10,12,3,4,5,6,7,8,7}; 
+  float InArray[16] = {2,4,5,6,7,8,9,10,12,3,4,5,6,7,8,7}; 
   REQUIRE((findMovingAverageFromBmsData(InArray,16)) == 6);
   
 }
